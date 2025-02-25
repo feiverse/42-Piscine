b2e7bb61aca930920c0fac1f678af00166d3d348
feiverse <186351189+feiverse@users.noreply.github.com>
2025-02-25 16:09:21 +0100

    Create ex01_ft_ultimate_ft.c

---
+++ c01/ex01_ft_ultimate_ft.c
@@ -0,0 +1,32 @@
+/*
+#include <stdio.h>
+*/
+
+void    ft_ultimate_ft(int *********nbr)
+{
+        *********nbr = 42;
+}
+
+/*
+int     main(void)
+{
+        int     v;
+
+        v = 21;
+
+        int     *nbr1 = &v;
+        int     **nbr2 = &nbr1;
+        int     ***nbr3 = &nbr2;
+        int     ****nbr4 = &nbr3;
+        int     *****nbr5 = &nbr4;
+        int     ******nbr6 = &nbr5;
+        int     *******nbr7 = &nbr6;
+        int     ********nbr8 = &nbr7;
+        int     *********nbr9 = &nbr8;
+
+        printf("v = %d\n", v);
+        ft_ultimate_ft(nbr9);
+        printf("v = %d\n", v);
+        return (0);
+}
+*/
