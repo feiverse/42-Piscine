80d8da9ddfa3ccb38cfb84e6a262d1200712283d
feiverse <186351189+feiverse@users.noreply.github.com>
2025-02-25 16:06:30 +0100

    Create ex00-ft_ft.c

---
+++ c01/ex00-ft_ft.c
@@ -0,0 +1,17 @@
+void    ft_ft(int *nbr)
+{
+        *nbr = 42;
+}
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int v;
+    
+    v = 7;
+    printf("%d\n", v);
+    ft_ft(&v);
+    printf("%d\n", v);
+}
+*/
